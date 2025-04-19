5f3d904850ba2a5de49779f0443a7d0f1acd5c19
Renan-Maestre <renan.maestre.dev@gmail.com>
2025-04-19 12:24:04 -0300

    feat: add routing with react-router-dom and implement task details page

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,36 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import AddTask from "./components/AddTask";
 import Tasks from "./components/Tasks";
 import { v4 } from "uuid";
 
 function App() {
-  const [tasks, setTasks] = useState([
-    {
-      id: 1,
-      title: "Estudar programação",
-      description: "Estudar programação para me tornar um porgramdor fullStack",
-      isCompleted: false,
-    },
-    {
-      id: 2,
-      title: "Estudar ingles",
-      description: "Para falar melhor ",
-      isCompleted: false,
-    },
-    {
-      id: 3,
-      title: "Estudar matemática",
-      description: "para fazer contas rapidas ",
-      isCompleted: false,
-    },
-  ]);
+  const [tasks, setTasks] = useState(
+    JSON.parse(localStorage.getItem("tasks")) || []
+  );
+
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }, [tasks]);
+
+  // Se você quiser pode usar as tarefas de uma api
+  // useEffect(() => {
+  //   const fetchTasks = async () => {
+  //     //chamar api
+  //     const response = await fetch(
+  //       "https://jsonplaceholder.typicode.com/todos?_limit=10",
+  //       {
+  //         method: "GET",
+  //       }
+  //     );
+
+  //     //Pegar os dados que ela retorna
+  //     const data = await response.json();
+
+  //     //armazenar/persistir esses dados no states
+  //     setTasks(data);
+  //   };
+  //   fetchTasks();
+  // }, []);
 
   function onTaskClick(taskId) {
     const newTasks = tasks.map((task) => {
@@ -39,7 +46,6 @@ function App() {
   }
 
   function onDeleteTaskClick(taskId) {
-    console.log("Deletando:", taskId);
     const newTasks = tasks.filter((task) => task.id !== taskId);
     setTasks(newTasks);
   }
@@ -55,7 +61,7 @@ function App() {
   }
 
   return (
-    <div className="w=screen h-screen bg-slate-500 flex justify-center p-6">
+    <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
       <div className="w-[500px] space-y-4">
         <h1 className="text-3xl text-slate-100 font-bold text-center">
           Gerenciador de tarefas
